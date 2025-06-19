1279b6bd16b397c033bacc22e1521c4a71baa597
Michal Glaz <michal.glaz@op.pl>
2025-06-19 06:33:25 +0200

    First function of Modul3 is halfway done

--- Moduł3.py
+++ Moduł3.py
@@ -7,18 +7,42 @@ import urllib.request
 
 def req(file):
 #    resp = r.get("https://openlibrary.org/search.json?title=the+lord+of+the+rings&author=J.R.R.%20Tolkien&fields=author_key/").json()
+#    print(file.head(10))
+#    t=[]
+#    firstName=[]
+#    surname=[]
+#    for col in file.columns:
+#        if 'Title' in col:
+#            t = file[col]
+#        elif 'Name' in col:
+#            firstName = file[col]
+#        elif 'Surname' in col:
+#            surname = file[col]
 
-    urldata = dict()
-    with urllib.request.urlopen("https://openlibrary.org/search.json?title=the+lord+of+the+rings&author=J.R.R.%20Tolkien&fields=title,publisher,author_name") as url:
-        data = json.load(url)
-#        print(data)
-        urldata = dict(data)
-#        print(urldata)
-#    print(urldata['docs'])
-    urldata = urldata['docs']
-    urldata = list(urldata[0].values())[1]
-#    urldata = urldata[]
-    print(urldata[0])
+
+#    print(title)
+    for index,row in file.iterrows():
+        print(row["Title"])
+        print(row["Name"])
+        print(row["Surname"])
+        t =row["Title"].replace(' ','%20')
+        firstName =row["Name"].replace(' ','%20')
+        surname =row["Surname"].replace(' ','%20')
+#        t='the+lord+of+the+rings'
+        urldata = dict()
+        try:
+            with urllib.request.urlopen(f'https://openlibrary.org/search.json?title={t}&author={firstName}%20{surname}&fields=title,publisher,author_name') as url:
+                data = json.load(url)
+#               print(data)
+                urldata = dict(data)
+#               print(urldata)
+#           print(urldata['docs'])
+            urldata = urldata['docs']
+            urldata = list(urldata[0].values())[1]
+#       urldata = urldata[]
+            print(urldata[0])
+        except:
+            print("Podane złe dane")
 
     with urllib.request.urlopen("https://openlibrary.org/search.json?title=the+lord+of+the+rings&author=J.R.R.%20Tolkien&fields=author_key") as url:
         data = json.load(url)
