c43f184d9def3764e4478b4c8eac8ab9755d983e
Michal Glaz <michal.glaz@op.pl>
2025-06-22 19:43:48 +0200

    Seperated serialize method into load,save methods in Modul5

--- Moduł5.py
+++ Moduł5.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-def serialize(data_frame,conf)-> pd.DataFrame:
+def load(conf)-> pd.DataFrame:
+    config_json = conf
+    print(pd.read_pickle(f'{config_json["extent"]}'))
+    print("Pickling read Modul5")
+    return pd.read_pickle(f'{config_json["extent"]}')
+def save(data_frame,conf):
     config_json = conf
     data_frame.to_pickle(f'{config_json["extent"]}')
     print("After pickling Modul5")
     print(pd.read_pickle(f'{config_json["extent"]}'))
     print("Pickling read Modul5")
-    return pd.read_pickle(f'{config_json["extent"]}')
--- PPY_Project.py
+++ PPY_Project.py
@@ -3,7 +3,8 @@ from Moduł1 import loadData
 from Moduł2 import retransform
 from Moduł3 import readditional_data
 from Moduł4 import my_api
-from Moduł5 import serialize
+from Moduł5 import load
+from Moduł5 import save
 from flask import Flask
 import pandas as pd
 
@@ -11,13 +12,16 @@ with open("config.json") as config:
     conf = json.load(config)
     print("Loaded config.json")
 
+#bez sprawdzania czy jest plik
+pd = load(conf)
+
 app = Flask(__name__)
-data = loadData(conf)
-transform = retransform(data,conf)
+#data = loadData(conf)
+#transform = retransform(data,conf)
 #print(transform)
-additional = readditional_data(transform,conf)
-my_api(app,additional)
-serialize(additional,conf)
+#additional = readditional_data(transform,conf)
+#my_api(app,additional)
+#save(data,conf)
 #Dodatkowy data frame aby łączyć wyniki i odwoływać się do jednego pliku
 #PLus lupa dla wyczekiwania na nowe pliki
 if __name__ == '__main__':
